0dca7e9e2bb8cf016c7cc3c219c08c6e7a206815
yuchenH <huangyuchen@caiqr.com>
2018-11-07 17:57:21 +0800

    commi

--- TSWebViewController/code/TSWebViewController+WebCallNative.swift
+++ TSWebViewController/code/TSWebViewController+WebCallNative.swift
@@ -10,63 +10,63 @@ import UIKit
 
 /// JS 调用 Native
 
-public extension TSWebViewController {
-    
-    /// JS 调用 Native方法
-    @objc public func jsCallNative(name: String, body: Any) {
-        
-        switch name {
-        case ts_webCallShare:
-            self.webCallShare(body: body)
-        case ts_webCallLoginStatus:
-            self.webCallLoginStatus(body: body)
-        case ts_webCallPayment:
-            self.webCallPayment(body: body)
-        case ts_webCallNativeRouter:
-            self.webCallNativeRouter(body: body)
-        case ts_webCallClosePage:
-            self.webCallClosePage(body: body)
-        case ts_webCallCopyText:
-            self.webCallCopyText(body: body)
-        case ts_webCallError:
-            self.webCallError(body: body)
-        case ts_webCallScreenCapture:
-            self.webCallScreenCapture(body: body)
-        default:
-            break
-        }
-        
-    }
-    
-    /// 分享
-    @objc public func webCallShare(body: Any) {}
-    
-    /// 登录状态
-    @objc public func webCallLoginStatus(body: Any) {}
-    
-    /// 调用支付
-    @objc public func webCallPayment(body: Any) {}
-    
-    /// 跳转本地router
-    @objc public func webCallNativeRouter(body: Any) {}
-    
-    /// 关闭页面
-    @objc public func webCallClosePage(body: Any) {}
-    
-    /// 复制文本
-    @objc public func webCallCopyText(body: Any) {}
-    
-    /// 异常交互
-    @objc public func webCallError(body: Any) {}
-    
-    /// 截取屏幕
-    @objc public func webCallScreenCapture(body: Any) {}
-    
-    /// 注册jS call native 的方法名
-    @objc public func registerJSCallNativeName() -> [String]? {
-        
-        return nil
-    }
-    
-}
-
+//public extension TSWebViewController {
+//    
+//    /// JS 调用 Native方法
+//    public func jsCallNative(name: String, body: Any) {
+//        
+//        switch name {
+//        case ts_webCallShare:
+//            self.webCallShare(body: body)
+//        case ts_webCallLoginStatus:
+//            self.webCallLoginStatus(body: body)
+//        case ts_webCallPayment:
+//            self.webCallPayment(body: body)
+//        case ts_webCallNativeRouter:
+//            self.webCallNativeRouter(body: body)
+//        case ts_webCallClosePage:
+//            self.webCallClosePage(body: body)
+//        case ts_webCallCopyText:
+//            self.webCallCopyText(body: body)
+//        case ts_webCallError:
+//            self.webCallError(body: body)
+//        case ts_webCallScreenCapture:
+//            self.webCallScreenCapture(body: body)
+//        default:
+//            break
+//        }
+//        
+//    }
+//    
+//    /// 分享
+//    public func webCallShare(body: Any) {}
+//    
+//    /// 登录状态
+//    public func webCallLoginStatus(body: Any) {}
+//    
+//    /// 调用支付
+//    public func webCallPayment(body: Any) {}
+//    
+//    /// 跳转本地router
+//    public func webCallNativeRouter(body: Any) {}
+//    
+//    /// 关闭页面
+//    public func webCallClosePage(body: Any) {}
+//    
+//    /// 复制文本
+//    public func webCallCopyText(body: Any) {}
+//    
+//    /// 异常交互
+//    public func webCallError(body: Any) {}
+//    
+//    /// 截取屏幕
+//    public func webCallScreenCapture(body: Any) {}
+//    
+//    /// 注册jS call native 的方法名
+//    public func registerJSCallNativeName() -> [String]? {
+//        
+//        return nil
+//    }
+//    
+//}
+//
--- TSWebViewController/code/TSWebViewController.swift
+++ TSWebViewController/code/TSWebViewController.swift
@@ -124,6 +124,64 @@ open class TSWebViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //MARK: - js call native
+    /// JS 调用 Native方法
+    public func jsCallNative(name: String, body: Any) {
+        
+        switch name {
+        case ts_webCallShare:
+            self.webCallShare(body: body)
+        case ts_webCallLoginStatus:
+            self.webCallLoginStatus(body: body)
+        case ts_webCallPayment:
+            self.webCallPayment(body: body)
+        case ts_webCallNativeRouter:
+            self.webCallNativeRouter(body: body)
+        case ts_webCallClosePage:
+            self.webCallClosePage(body: body)
+        case ts_webCallCopyText:
+            self.webCallCopyText(body: body)
+        case ts_webCallError:
+            self.webCallError(body: body)
+        case ts_webCallScreenCapture:
+            self.webCallScreenCapture(body: body)
+        default:
+            break
+        }
+        
+    }
+    
+    /// 分享
+    public func webCallShare(body: Any) {}
+    
+    /// 登录状态
+    public func webCallLoginStatus(body: Any) {}
+    
+    /// 调用支付
+    public func webCallPayment(body: Any) {}
+    
+    /// 跳转本地router
+    public func webCallNativeRouter(body: Any) {}
+    
+    /// 关闭页面
+    public func webCallClosePage(body: Any) {}
+    
+    /// 复制文本
+    public func webCallCopyText(body: Any) {}
+    
+    /// 异常交互
+    public func webCallError(body: Any) {}
+    
+    /// 截取屏幕
+    public func webCallScreenCapture(body: Any) {}
+    
+    /// 注册jS call native 的方法名
+    public func registerJSCallNativeName() -> [String]? {
+        
+        return nil
+    }
+    
 }
 
 extension TSWebViewController: WKScriptMessageHandler, WKNavigationDelegate, WKUIDelegate {
